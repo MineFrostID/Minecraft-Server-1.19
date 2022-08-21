2a633bd1427bbcc7610f4aaf31af55935f029797
MineFrostID <obenunity56@gmail.com>
2022-08-22 01:47:59 +0700

    Update Server

---
+++ push.bat
@@ -0,0 +1,4 @@
+
+git add *
+git commit -m "Update Server"
+git push origin master
--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
-git pull
+git pull origin master
 java -Xmx6192M -Xms6192M -jar server.jar nogui
-PAUSE
 git add *
 git commit -m "Update Server"
 git push origin master
+PAUSE
